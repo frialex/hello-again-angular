dde1d775e1e8136018376092cba343ffddb24ba0
Frison B Alexander <fri.b.alex@gmail.com>
2018-01-29 12:31:05 -0500

    ngFor

--- HelloAgain/src/app/second/second.component.ts
+++ HelloAgain/src/app/second/second.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponent implements OnInit {
 
+  listOfStuff = [{href: "https://angular.io/tutorial", text: "Tour of Heroes"},
+                 {href: "https://github.com/angular/angular-cli/wiki", text: "CLI Documentation"},
+                 {href: "https://angular.io/guide/displaying-data", text: "Displaying Data"}]
+
   constructor() { }
 
   ngOnInit() {
